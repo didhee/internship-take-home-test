674a88e1ca0cca56687b13387692d6fb411c7625
Onoriode Dafetta <onoriodedafetta@gmail.com>
2023-04-28 01:06:29 +0100

    some changes made to question 2

--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -1,4 +1,4 @@
-import  {useState} from "react";
+import { useState } from "react";
 
 function ReadMore({ text }) {
    // i created a state and set it's initial state to false.
@@ -7,7 +7,7 @@ function ReadMore({ text }) {
    return (
      // created an onClick function, such that when the read more is clicked, the state changes and it expands and reveals the description, on expansion, the state has changed to true, since it is a boolean in this case.
       // the slice method, I used it to shorten the words, so that the read more can be there and carry out it's function.
-    <p onClick={() => setReadMore(!readMore)}>
+    <p className="des" onClick={() => setReadMore(!readMore)}>
       {readMore ? text : text.slice(0, 80)}
       {!readMore && (
         <span className="expand">
--- src/components/ViewItems.jsx
+++ src/components/ViewItems.jsx
@@ -1,21 +1,22 @@
-import metaDataList from './../constants/constant';
-import ReadMore from './ReadMore';
+import metaDataList from "./../constants/constant";
+import ReadMore from "./ReadMore";
+// import iPhone from "../assets/images/img1.jpeg"
 
-
-
-export default function ViewItems () {
-   return (
-      //I called the metadata and mapped through each item to return the name and the description. I also called the Read More component here too.
-      metaDataList.map(item => (
-         <div className="grid">
-            {/* <img src={item.image} alt={item.name} /> */}
-            <h3>{item.name}</h3>
-            <ReadMore text={item.description}/>
-        </div>
-     ))
-  )
+export default function ViewItems() {
+  return (
+    <>
+        <h1>A Simple Display of Items</h1>
+        
+        {metaDataList.map((item) => (
+           <div className="grid">
+              <img src={item.picture} alt={item.name} />
+              <h3>{item.name}</h3>
+              <ReadMore text={item.description} />
+           </div>
+        ))}
+    </>
+  );
 }
-
 
 // What I did was to break this into different components, as react is a component based library to ensure reusability. I broke the "read more" feature into another componrnt.
 // I also put all the data or constants I am to use into a js file so as my code base won't be clogged and for easy usability and reading
